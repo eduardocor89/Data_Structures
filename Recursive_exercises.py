fe631952adec43dc8609e5a09c5db3b4f86ff4f8
Eddie Cordero <78165600+eduardocor89@users.noreply.github.com>
2022-06-29 11:16:56 -0400

    Update Recursive_exercises.py

--- Recursive_exercises.py
+++ Recursive_exercises.py
@@ -20,3 +20,38 @@ def factorial(n):
   if n <= 1:    
     return 1  
   return n * factorial(n - 1)
+
+
+'''
+Let us now depict the fibonacci sequence in both recursive and iterative fashion.
+'''
+
+# ITERATIVE
+def fibonacci(n):
+  list = [0,1]
+  if n < 0:
+    return ValueError("Input 0 or greater, please")
+  elif n <= 1:
+    return n
+  else:
+    while len(list) <= n:
+      list.append((int(list[-2])) + (int(list[-1])))
+      print(list)
+  return list[n]
+
+# test cases
+print(fibonacci(3) == 2)
+print(fibonacci(7) == 13)
+print(fibonacci(0) == 0)
+
+# RECURSIVE
+def fibonacci(n):
+  if n < 0:
+    ValueError("Input 0 or greater only!")
+  if n <= 1:
+    return n
+  return fibonacci(n - 1) + fibonacci(n - 2)
+# test cases 
+fibonacci(3)
+fibonacci(7)
+fibonacci(0)
