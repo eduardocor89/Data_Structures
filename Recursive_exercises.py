d54f96da036fc80ebb9dc9d251abfc0baa677652
Eddie Cordero <78165600+eduardocor89@users.noreply.github.com>
2022-07-11 18:36:39 -0400

    Update Recursive_exercises.py

--- Recursive_exercises.py
+++ Recursive_exercises.py
@@ -177,3 +177,25 @@ print(is_palindrome("Was it a car or a cat I saw") == True)
 
 
 
+'''
+Multiplication substitute:
+We're making a substitute for multiplication as if python didn't have the expression built in. 
+'''
+
+# ITERATIVE
+def multiplication(num_1, num_2):
+  result = 0
+  for count in range(0, num_2):
+    result += num_1
+  return result
+
+# RECURSIVE 
+def multiplication(num1, num2):
+  if num1 == 0 or num2 == 0:
+    return 0
+  
+  return num1 + multiplication(num1, num2 - 1)
+# test cases
+print(multiplication(3, 7) == 21)
+print(multiplication(5, 5) == 25)
+print(multiplication(0, 4) == 0)
